db7134f756067eea76a953972fe382ca894129bd
Nikiw <Nikiw@NIKIWE_MBHIZA>
2024-01-18 08:58:23 +0200

    Add project files.

---
+++ Form1.vb
@@ -0,0 +1,27 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        ListBox1.Items.Add("Redmi")
+        Dim myitems
+        myitems = InputBox("enter your favourite phone")
+        ListBox1.Items.Add(myitems)
+    End Sub
+
+    Private Sub ListBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBox1.SelectedIndexChanged
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim myitem2
+        myitem2 = InputBox("enter a phone to remove")
+
+        ListBox1.Items.Remove(myitem2)
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        ListBox1.Items.Remove(ListBox1.SelectedItem)
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        ListBox1.Items.Clear()
+    End Sub
+End Class
